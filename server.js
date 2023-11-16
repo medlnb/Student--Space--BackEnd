110f2c61f8ddd250d90df1c3cb78403c4fb45f17
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-16 17:45:29 +0100

    adding sockets

--- server.js
+++ server.js
@@ -1,38 +1,52 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const Authrouter = require('./Routers/User');
-const Student = require('./Routers/Student');
-const Task = require('./Routers/Task');
-const Schedule = require('./Routers/Schedule');
-const File = require('./Routers/File');
-const Announcement = require('./Routers/Announcement');
-const { createServer } = require('http'); // Import the createServer function from the http module
-const { Server } = require('socket.io');
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const Authrouter = require('./Routers/User')
+const Student = require('./Routers/Student')
+const Task = require('./Routers/Task')
+const Schedule = require('./Routers/Schedule')
+const File = require('./Routers/File')
+const Announcement = require('./Routers/Announcement')
+const { Server } = require('socket.io')
+const {createServer} = require("http")
 
 require("dotenv").config()
 
 const app = express()
 
-const server = createServer(app); // Create an HTTP server and pass it the Express app
-const io = new Server(server); // Attach Socket.IO to the HTTP server
+const httpServer = createServer()
+const io = new Server(httpServer, {
+  cors: {
+    origin:"*",
+  },
+})
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
+// io.use(cors())
 
-app.use("/api/user", Authrouter);
-app.use("/api/student", Student); 
-app.use("/api/task", Task);
-app.use("/api/schedule", Schedule);
-app.use("/api/file", File);
-app.use("/api/announcement", Announcement);
+app.use("/api/user", Authrouter)
+app.use("/api/student", Student)
+app.use("/api/task", Task)
+app.use("/api/schedule", Schedule)
+app.use("/api/file", File)
+app.use("/api/announcement", Announcement)
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+  
+  
+  socket.on("msg", (data) => {
+    io.emit("NewNotification", data)
+  })
+  
+})
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`Connected to the database and listening on port ${process.env.PORT}`);
-  });
-});
+  app.listen(process.env.PORT, () => {
+    console.log(`Connected to the database and listening on port ${process.env.PORT}`)
+  })
+})
+
+httpServer.listen(3001, () => {
+  console.log("listening to 3001")
+})

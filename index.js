618a84a1adf13c719dea54100e07b8a703438e8d
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-24 15:21:52 +0100

    fixing cors bug

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const io = new Server(httpServer, {
 
 app.use(express.json())
 app.use(cors())
-// io.use(cors())
+io.use(cors())
 
 app.use("/api/user", Authrouter)
 app.use("/api/student", Student)
@@ -32,9 +32,6 @@ app.use("/api/schedule", Schedule)
 app.use("/api/file", File)
 app.use("/api/announcement", Announcement)
 
-//  io.on('connection', (socket) => {
-//     socket.emit("NewNotification","hello")
-//   })
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   app.listen(process.env.PORT, () => {

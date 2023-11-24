b4bd9deffa4b4426a53515e090613db658d00ba5
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-24 18:52:45 +0100

    removing sockets

--- index.js
+++ index.js
@@ -24,17 +24,6 @@ app.use("/api/schedule", Schedule)
 app.use("/api/file", File)
 app.use("/api/announcement", Announcement)
 
-// const {createServer} = require("http")
-// const { Server } = require("socket.io");
-// const httpServer = createServer();
-// const io = new Server(httpServer, {
-//   cors: {
-//     origin: "https://s-s-s.vercel.app"
-//   }
-// });
-
-// io.listen(3001);
-
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   app.listen(process.env.PORT, () => {

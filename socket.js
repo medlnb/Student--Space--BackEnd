c09e45e9f5fa4a519ef4545fad344397ddda32b3
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-24 16:27:17 +0100

    fixing cors error 12

--- socket.js
+++ socket.js
@@ -1,15 +1,16 @@
-// const { Server } = require('socket.io');
-// const { createServer } = require('http');
+const { Server } = require('socket.io');
+const { createServer } = require('http');
 
 
-// const httpServer = createServer();
-// const io = new Server(httpServer, {
-//   cors: {
-//     origin: '*',
-//   },
-// });
-// httpServer.listen(3001, () => {
-//   console.log('listening to 3001');
-// });
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+httpServer.listen(3001, () => {
+  console.log('listening to 3001');
+});
 
-// module.exports = io;
+module.exports = io;

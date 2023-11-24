b126f646c5dd9501256ef4b49edc9b6f7ca12ac5
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-24 16:21:58 +0100

    fixing cors error 8

--- socket.js
+++ socket.js
@@ -1,10 +1,13 @@
 const { Server } = require('socket.io');
 const { createServer } = require('http');
-const cors = require('cors');
+
 
 const httpServer = createServer();
-const io = new Server(httpServer);
-io.use(cors())
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
 httpServer.listen(3001, () => {
   console.log('listening to 3001');
 });

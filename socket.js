3d7e42bc188d8a0552e7a8fbfa6fa5b583c59a55
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-24 16:11:59 +0100

    fixing cors error 1

--- socket.js
+++ socket.js
@@ -2,11 +2,12 @@ const { Server } = require('socket.io');
 const { createServer } = require('http');
 
 const httpServer = createServer();
-const io = new Server(httpServer, {
-  cors: {
-    origin: 'https://s-s-s.vercel.app',
-  },
-});
+const io = new Server(httpServer);
+io.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+})
 httpServer.listen(3001, () => {
   console.log('listening to 3001');
 });

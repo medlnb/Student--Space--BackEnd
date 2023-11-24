4743c90a15602c5add880d6e4c4d5b85012d763a
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-24 16:22:38 +0100

    fixing cors error 9

--- Controllers/Announcement.js
+++ Controllers/Announcement.js
@@ -1,5 +1,5 @@
   const Announcement = require("../Models/Announcement")
-// const io = require('../socket');
+const io = require('../socket');
 
   const CreateAnnouncement = async (req, res) => {
     const { Publisher, Content } = req.body
@@ -10,9 +10,9 @@
     if (!announcement)
       return res.status(404).json({ err: "Error Creating the Announcement" })
     
-  //   io.on('connection', (socket) => {
-  //   socket.emit("NewNotification","hello")
-  // })
+    io.on('connection', (socket) => {
+    socket.emit("NewNotification","hello")
+  })
     return res.status(201).json(announcement)
   }
 

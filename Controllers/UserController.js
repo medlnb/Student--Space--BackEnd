f270e8269570458f05da7dffa5ddc9aa996829a4
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-12-03 13:54:10 +0100

    adjusting login for teacher

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -8,6 +8,8 @@ const CreateTeacher = async (req, res) => {
     return res.status(409).json({ err: "Failled creating Teacher" })
   return res.status(200).json({ username:username, email, isTeacher: true  })
 }
+
+
 const login = async (req, res) => {
   const { email, password } = req.body
   const user = await User.find({ email }) 
@@ -16,10 +18,15 @@ const login = async (req, res) => {
     return res.status(404).json({ MailErr: "Email does'nt exist" })
   
   if (user[0].isTeacher) {
-    let teachers = user.map(usr => (usr.username))
-    if (teachers.length === 1)
-      teachers = teachers[0]
-    return res.status(201).json({ username:teachers , email, isTeacher: true })
+    let modules = ""
+    user.map(usr => {
+      modules = modules+usr.email+"$$"
+    })    
+    return res.status(201).json({
+      username: user[0].username,
+      email: modules,
+      isTeacher: true
+    })
   }    
   if (password !== user[0].password)
     return res.status(404).json({ PwErr:"wrong password"})
